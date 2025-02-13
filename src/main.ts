9e9219cb0d6a28f8cf6380e154c7395186da342f
Diego-c-8 <diego.c.080808@gmail.com>
2025-02-13 20:36:28 -0300

    print for fuentes

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
   });
 
   await app.listen(port);
-  Logger.log(`Application is running on: http://localhost:${port}`);
+  Logger.log(`Fuentes disponibles${origin}`);
 }
 
 bootstrap();
